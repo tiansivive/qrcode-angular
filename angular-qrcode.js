97d5189d9b6721aff55b7d310c221ab6b6f0faa8
Isidro Vila Verde <jvverde@gmail.com>
2014-08-08 00:00:28 +0100

    add callback and encoding mode

--- angular-qrcode.js
+++ angular-qrcode.js
@@ -13,21 +13,24 @@ angular.module('tiago.qrcode', [])
     return {
       restrict: 'E',
       template: '<canvas></canvas>',
-      link: function(scope, element, attrs) {
+      scope: { //http://stackoverflow.com/questions/15896985/angular-js-callback-function-inside-directive-attr-defined-in-different-attr
+        callback: '&done'
+      }
+      ,link: function(scope, element, attrs) {
 
-        console.log("Element = " + element);
         var domElement = element[0],
         canvas = element.find('canvas')[0],
         trim_regex = /^(\s+\n+)|(\s+|\n+)$/g,
         error,
         ECL,
-        data,
+        data =  '',
         size,
         background,
         foreground,
+        qr = new JSQR(),
         scale,
         bg,
-        qr = new JSQR(),
+        emode = 'UTF8_SIGNATURE', 
         setECL = function(value) {
           ECL = value in levels ? value : 'L';
         },
@@ -58,7 +61,7 @@ angular.module('tiago.qrcode', [])
 
 
           var code = new qr.Code();
-          code.encodeMode = code.ENCODE_MODE.UTF8_SIGNATURE;
+          code.encodeMode = code.ENCODE_MODE[emode];
           code.version = code.DEFAULT;
           if(ECL == 'L'){
             code.errorCorrection = code.ERROR_CORRECTION.L;
@@ -81,7 +84,7 @@ angular.module('tiago.qrcode', [])
 
           var matrix = new qr.Matrix(input, code);
 
-          if (scale) matrix.scale = scale;    
+          if (scale) matrix.scale = scale;   //TODO change with size option   
 
           canvas.setAttribute('width', matrix.pixelWidth);
           canvas.setAttribute('height', matrix.pixelWidth);
@@ -92,7 +95,7 @@ angular.module('tiago.qrcode', [])
           }
           ctx.fillStyle = 'rgb(0,0,0)'; //TODO change with colors
           matrix.draw(canvas, 0, 0);
-          matrix.draw(canvas, 0, 0);
+          scope.callback({canvas:canvas});
         };
 
 
@@ -124,7 +127,7 @@ angular.module('tiago.qrcode', [])
 
           setSize(value);
           create_code();
-        });
+        });        
 
         attrs.$observe('scale', function(value) {
           if (!value) {
@@ -132,7 +135,7 @@ angular.module('tiago.qrcode', [])
           }
           scale = value;
           create_code();
-        });        
+        });
 
         attrs.$observe('bg', function(value) {
           if (!value) {
@@ -141,6 +144,15 @@ angular.module('tiago.qrcode', [])
           bg = value;
           create_code();
         });
+
+        attrs.$observe('emode', function(value) {
+          if (!value) {
+            return;
+          }
+          emode = value;
+          create_code();
+        });
+        
 /*
         setECL(attrs.ECL);
         setSize(attrs.size);
